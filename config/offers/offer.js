f1004a5cf044f1466e1db72555cff95c7584363f
chmoreau <charles.moreau@insa-lyon.fr>
2016-05-04 01:59:18 +0200

    regex on all offers

--- config/offers/offer.js
+++ config/offers/offer.js
@@ -102,7 +102,7 @@ exports.confirm_offer = function(data, callback){
 exports.all_offers = function(data, callback){
 	var arrival = data.arrival;
 	var departure = data.departure;
-	var query = {'ride.arrival.name': arrival, 'ride.departure.name': departure};
+	var query = { 'ride.departure.name' :  {$regex : '.*' + departure + '.*', $options: 'i'} , 'ride.arrival.name' :  {$regex : '.*' + arrival + '.*', $options: 'i'}};
 	var userMap = [];
 	offer.find(query).then(function(response){
 		console.log(response);
